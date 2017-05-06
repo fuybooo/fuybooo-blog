0ee095a77e424759e43cfa39ffeb5a28db33db32
fuybooo <fuybooo@qq.com>
2017-05-07 00:45:48 +0800

    注册,登录

--- routes/login.js
+++ routes/login.js
@@ -10,20 +10,13 @@ router.post('/', function (req, res, next) {
     var username = req.body.username;
     hasher.update(req.body.password);
     var hashmsg = hasher.digest('hex');
-    console.log(hashmsg);
     db.executeSql(db.connect(), 'select * from t_user where user_name = \'' + username + '\'', function(result){
-        console.log('执行sql', result);
-        console.log('typeof result', typeof result);
-        console.log('JSON.stringify(result)', JSON.stringify(result) == '');
-        console.log('result ===1 ', result == ''); // 验证得到此方法可以判断是否查询到值了
-        console.log('result ===2 ', !result == true);
-        console.log('result ===3 ', result == false);
-        console.log('result ===4 ', !result == false);
         var data = {
             code: 0,
-            msg: '登录成功!'
+            msg: '登录成功!',
+            data: result
         };
-        if(result == ''){
+        if(!result){
             data = {
                 code: 1,
                 msg: '用户名不存在!'
--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,5 @@
 var UUID = require('node-uuid');
+var db = require('../public/javascripts/common/db-connect');
 var express = require('express');
 var crypto = require('crypto');
 var router = express.Router();
@@ -9,30 +10,29 @@ router.post('/', function (req, res, next) {
     var hasher = crypto.createHash('md5');
     hasher.update(password);
     var hashmsg = hasher.digest('hex');
-    console.log(hashmsg);
     var id = UUID.v1();
     var date = new Date();
-    var userId = (date.getMilliseconds() + '').slice(-10);
+    var userId = date.getTime();
     var userState = 1;
     var userEntryDate = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + (date.getDate() + 1)).slice(-2);
-    db.executeSql(db.connect(),
-        'insert into t_user(id, user_id, user_name, user_password,user_state, user_entry_date) values(' +
-        id + userId + username + hashmsg + userState + userEntryDate +
-        ')',
-        function(result){
-            console.log(result);
-            var data = {
-                code: 0,
-                msg: '注册成功!'
-            };
-            if(result == ''){
-                data = {
-                    code: -1,
-                    msg: '注册失败!'
-                }
+    var sql = 'insert into t_user(id, user_id, user_name, user_password,user_state, user_entry_date) values(' +
+        '\'' + id+ '\',\'' + userId + '\',\'' + username+ '\',\'' + hashmsg+ '\',' + userState+ ',\'' + userEntryDate + '\')';
+    console.log('sql:', sql);
+    var client = db.connect();
+    db.executeSql(client, sql, function (result) {
+        var data = {
+            code: 0,
+            msg: '注册成功!'
+        };
+        if (!result) {
+            data = {
+                code: -1,
+                msg: '注册失败!'
             }
-            res.end(JSON.stringify(data));
+        }
+        res.end(JSON.stringify(data));
     });
+    client.end();
 });
 
 module.exports = router;
