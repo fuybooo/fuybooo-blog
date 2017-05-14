4eb290e6774a9e62a4bd425632fb6f2146e0cb4e
fuybooo <fuybooo@qq.com>
2017-05-14 22:49:07 +0800

    快速编辑--bootstrap-table单元格编辑内容

--- routes/user.js
+++ routes/user.js
@@ -4,30 +4,52 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    var where = '';
-    var username = req.query.username;
-    console.log('用户名',username);
-    if(username){
-        where += 'user_name = \'' + username + '\'';
-    }
-    var sql = 'select * from t_user' + where + ' order by user_name asc';
-    console.log('sql',sql);
-    db.executeSql(db.connect(), sql, function(result){
-        console.log('执行sql', result);
-        var data = {
-            code: 0,
-            msg: '查询完成',
-            data: result
-        };
-        if(!result){
-            data = {
-                code: 1,
-                msg: '没有查询到数据!',
-                data: []
-            };
+        
+        var where = '';
+        var username = req.query.username;
+        if (username) {
+            where += 'user_name = \'' + username + '\'';
         }
-        res.end(JSON.stringify(data));
-    });
+        var sql = 'select * from t_user' + where + ' order by user_name asc';
+        db.executeSql(db.connect(), sql, function (result) {
+            var data = {
+                code: 0,
+                msg: '查询完成',
+                data: result
+            };
+            if (!result) {
+                data = {
+                    code: 1,
+                    msg: '没有查询到数据!',
+                    data: []
+                };
+            }
+            res.end(JSON.stringify(data));
+        });
+});
+
+router.post('/', function (req, res, next) {
+    
+    var action = req.body.action;
+    if (action === 'quickEdit') {
+        var sql = 'update t_user set ' + req.body.field + ' = \'' + req.body.value + '\' where id = \'' + req.body.id + '\'';
+        db.executeSql(db.connect(), sql, function (result) {
+            var data = {
+                code: 0,
+                msg: '修改成功!',
+                data: result
+            };
+            if(!result){
+                data = {
+                    code: 1,
+                    msg: '修改失败!'
+                };
+            }
+            res.end(JSON.stringify(data));
+        });
+    } else {
+    
+    }
 });
 
 module.exports = router;
